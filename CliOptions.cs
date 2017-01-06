e373830296a38c0c12f0b349b84808e22ee7c773
John Adams <john@opsgility.com>
2017-01-06 09:36:55 -0600

    Add initial project with support for netcoreapp1.1 and net462

---
+++ CliOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace keyvaultpfxtool
+{
+    public class CliOptions
+    {
+        [Option('r', "read", Required = true, HelpText = "Input file to be processed.")]
+        public string Filename { get; set; }
+
+        [Option(HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using CommandLine;
+using keyvaultpfxtool;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var parsedOptions = Parser.Default.ParseArguments<CliOptions>(args);
+
+            parsedOptions.WithParsed(opts =>
+            {
+                Console.WriteLine($"Reading {opts.Filename}.");
+
+                var jsonText = File.ReadAllText(opts.Filename);
+                var jsonInput = JsonConvert.DeserializeObject<JObject>(jsonText);
+
+                try
+                {
+                    if (jsonInput["dataType"].ToString().ToLower() != "pfx")
+                    {
+                        Console.WriteLine("Json file does not contain a pfx certificate.");
+                    }
+                    else
+                    {
+                        var pfxData = jsonInput["data"].ToString();
+                        var fileName = $"{opts.Filename}.pfx";
+                        File.WriteAllText(fileName, pfxData);
+                        Console.WriteLine($"Generated {fileName}");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine($"Could not read {opts.Filename}");
+                }
+
+            });
+
+        }
+    }
+}
